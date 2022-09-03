24faa72b05127645bcb2991c6ee9a650cc2574e7
AenclaveGames <55506834+IlyaChichkov@users.noreply.github.com>
2022-09-03 21:57:28 +0300

    init

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        './src/**/*.{js,ts,tsx,jsx,css}'
+    ],
+    theme: {
+        extend: {
+            colors: {
+                'bg-white': '#E6F0FD',
+                'text-black': '#070B2D',
+                'text-title': '#3847CC',
+                'main': '#5667FA',
+                'secondary': '#9BC9FF',
+            },
+        },
+        fontFamily: {
+            raleway: ['Raleway', 'sans-serif'],
+            rb: ['Roboto', 'sans-serif'],
+            rbMono: ['Roboto Mono', 'monospace'],
+            rubik: ['Rubik', 'sans-serif'],
+        }
+    },
+    plugins: [],
+}
